6970cb1f96ef2ca7e2017087126320800fcd43ff
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-11-15 15:39:32 -0300

    Updated hsdescr

--- libs/hsdescr.h
+++ libs/hsdescr.h
@@ -10,29 +10,38 @@
 
 #include "radio.h"
 
-#define configHSDESCR_RPROT_CMD_SIZE        	configRADIO_CMD_SIZE
-#define configHSDESCR_RPROT_DATA_SIZE       	configRADIO_DATA_SIZE
-#define configHSDESCR_RPROT_PAYLOAD_SIZE    	configRADIO_PAYLOAD_SIZE
+#define configHSDESCR_RPROT_CMD_SIZE        		configRADIO_CMD_SIZE
+#define configHSDESCR_RPROT_DATA_SIZE       		configRADIO_DATA_SIZE
+#define configHSDESCR_RPROT_PAYLOAD_SIZE    		configRADIO_PAYLOAD_SIZE
 
-#define configHSDESCR_RPROT_CMD             	0
-#define configHSDESCR_RPROT_REG             	1
+#define configHSDESCR_RPROT_CMD             		0
+#define configHSDESCR_RPROT_REG             		1
 
 
-#define configHSDESCR_SPROT_RADIO_COMM_WR_ERROR	1
-#define configHSDESCR_SPROT_RADIO_COMM_RD_ERROR	2
-#define configHSDESCR_SPROT_RADIO_COMM_OK		0xAA
+#define configHSDESCR_SPROT_GARMO_DO_READ			0x00
+#define configHSDESCR_SPROT_GARMO_DO_WRITE			0x01
 
-#define configHSDESCR_GARMO_REG_STATUS      	0
-#define configHSDESCR_GARMO_REG_ADC         	1
-#define configHSDESCR_GARMO_REG_DO          	2
+#define configHSDESCR_SPROT_RADIO_COMM_WR_ERROR		1
+#define configHSDESCR_SPROT_RADIO_COMM_RD_ERROR		2
+#define configHSDESCR_SPROT_RADIO_COMM_OK			0xAA
 
-#define configHSDESCR_GARMO_REG_DO_RESET    	0x00
-#define configHSDESCR_GARMO_REG_DO_SET      	0x01
+#define configHSDESCR_GARMO_REG_STATUS      		0
+#define configHSDESCR_GARMO_REG_ADC         		1
+#define configHSDESCR_GARMO_REG_DO          		2
 
-#define configHSDESCR_GARMO_CMD_READ        	0x00
-#define configHSDESCR_GARMO_CMD_WRITE       	0x01
+#define configHSDESCR_GARMO_REG_DO_RESET    		0x00
+#define configHSDESCR_GARMO_REG_DO_SET      		0x01
 
-#define configHSDESCR_GARMO_ADC_CHANNELS    	4
-#define configHSDESCR_GARMO_ADC_INPUTS      	((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3))
+#define configHSDESCR_GARMO_CMD_READ        		0x00
+#define configHSDESCR_GARMO_CMD_WRITE       		0x01
+
+#define configHSDESCR_GARMO_ADC_CHANNELS    		4
+#define configHSDESCR_GARMO_ADC_INPUTS      		((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3))
+
+
+#define configHSDESCR_BSTA_SPROT_ID_GARMO_STATUS	0x00000001
+#define configHSDESCR_BSTA_SPROT_ID_GARMO_ADC		0x00000002
+#define configHSDESCR_BSTA_SPROT_ID_GARMO_DO		0x00000003
+
 
 #endif /* HSDESCR_H_ */

011242bd9f8a25b6d17a588719f5a1112798fbc6
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-12-07 19:53:45 -0300

    Initial startup files

---
+++ mcu/startup.c
@@ -0,0 +1,14 @@
+/*
+ * startup.c
+ *
+ *  Created on: Dec 7, 2019
+ *      Author: marco
+ */
+
+#include "startup.h"
+#include "stm32f10x.h"
+
+void startup (void){
+
+
+}
---
+++ mcu/startup.h
@@ -0,0 +1,15 @@
+/*
+ * startup.h
+ *
+ *  Created on: Dec 7, 2019
+ *      Author: marco
+ */
+
+#ifndef STARTUP_H_
+#define STARTUP_H_
+
+
+void startup(void);
+
+
+#endif /* STARTUP_H_ */

e9bcb3d54c033b12bb6439ed8ac3a38c2f7d9755
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2020-01-18 11:47:54 -0300

    Modified drivers include guards and added delays

---
+++ libs/delays.h
@@ -0,0 +1,15 @@
+/*
+ * delays.h
+ *
+ *  Created on: Dec 15, 2019
+ *      Author: marco
+ */
+
+#ifndef DELAYS_H_
+#define DELAYS_H_
+
+
+extern void delaysCycles(uint32_t);
+
+
+#endif /* DELAYS_H_ */
